b5b8835f25f6ee82d47cde5c6bc1ef83b9662cb1
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-06-13 19:25:52 +0530

    updates test

--- shipctl/x86_64/Ubuntu_16.04/test.sh
+++ shipctl/x86_64/Ubuntu_16.04/test.sh
@@ -1,37 +1,45 @@
+echo './shipctl get_git_changes --path="$HOME/s/node"'
 ./shipctl get_git_changes --path="$HOME/s/node"
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --resource="mygitRepo"'
 ./shipctl get_git_changes --resource="mygitRepo"
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --depth=1'
 ./shipctl get_git_changes --path="$HOME/s/node" --depth=1
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --directories-only'
 ./shipctl get_git_changes --path="$HOME/s/node" --directories-only
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --depth=1 --directories-only'
 ./shipctl get_git_changes --path="$HOME/s/node" --depth=1 --directories-only
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --depth=2 --directories-only'
 ./shipctl get_git_changes --path="$HOME/s/node" --depth=2 --directories-only
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --depth=3 --directories-only'
 ./shipctl get_git_changes --path="$HOME/s/node" --depth=3 --directories-only
 echo $?
 
 echo "---->>>>"
 
+echo './shipctl get_git_changes --path="$HOME/s/node" --depth=1 --directories-only --commit-range="HEAD..HEAD~100"'
 ./shipctl get_git_changes --path="$HOME/s/node" --depth=1 --directories-only --commit-range="HEAD..HEAD~100"
 echo $?
